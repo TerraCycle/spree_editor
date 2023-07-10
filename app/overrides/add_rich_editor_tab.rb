1b5d5f8fab142fe16f1faff9bea6a06c4a02ad08
ivan-sheremeta <iisheremeta@gmail.com>
2023-07-10 10:35:50 +0300

    Rails 7 support; Tinymce upgrade; Remove deface

--- app/overrides/add_rich_editor_tab.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-  name: 'add_rich_editor_tab',
-  insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-  text: '<%= configurations_sidebar_menu_item Spree.t(:rich_editor, scope: :editor_settings),
-             spree.edit_admin_editor_settings_path %>'
-)
